a92a0f071adb6669db1ba901ef2c10fedc494352
Nicolas Oñate Oñate <ing.nionate@gmail.com>
2016-05-12 20:49:24 -0300

    Revert "Primer Commit"
    This reverts commit 035c373fe20240e6e189bc5a6f53ccaf778e32b1.

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1:3306;dbname=tesisdb',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
-];
